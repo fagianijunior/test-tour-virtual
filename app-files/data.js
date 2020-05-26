232e3f1d28f7691c5b96e726f9c630db9c3a1fb4
Carlos Fagiani junior <fagianijunior@gmail.com>
2020-05-26 17:21:55 -0300

    Initial commit

---
+++ app-files/data.js
@@ -0,0 +1,100 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-timothy-oldfield-luufnhochru-unsplash",
+      "name": "timothy-oldfield-luufnHoChRU-unsplash",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.6575398114348641,
+        "pitch": 0.03422617662888783,
+        "fov": 0.6300762889296663
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.605144988968113,
+          "pitch": -0.026844238291394262,
+          "rotation": 0,
+          "target": "1-bryan-goff-iuyhxaia8ea-unsplash"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bryan-goff-iuyhxaia8ea-unsplash",
+      "name": "bryan-goff-IuyhXAia8EA-unsplash",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 2.399973906485359,
+        "pitch": 0.007567676657535216,
+        "fov": 1.2498900900443681
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8733031917616607,
+          "pitch": -0.04759505295761812,
+          "rotation": 0,
+          "target": "0-timothy-oldfield-luufnhochru-unsplash"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.899667797249716,
+          "pitch": -0.043272319950979465,
+          "title": "Carro velho",
+          "text": "Quebrou em 1999"
+        },
+        {
+          "yaw": 1.9829345617155347,
+          "pitch": -0.12026120399307594,
+          "title": "Carro novo",
+          "text": "Comprei ontem, tem LED"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
